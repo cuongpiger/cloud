20cb30f2e9aac600aa665b143a8d0ff18b8c1251
Manh Cuong <cuongpigerr@gmail.com>
2022-12-22 22:43:11 +0700

    This is Manh Cuong's commit 🌻.

--- chap06/demo_01/app/identidock.py
+++ chap06/demo_01/app/identidock.py
@@ -1,21 +1,31 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
+import redis
 
 app = Flask(__name__)
 default_name = 'Joe Bloggs'
+salt = "UNIQUE_SALT"
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
     name = default_name
+
+    if request.method == 'POST':
+        name = request.form['name']
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
     header = '<html><head><title>Identidock</title></head><body>'
     body = '''<form method="POST">
-        Hello <input type="text" name="name" value="{}">
+        Hello <input type="text" name="name" value="{0}">
         <input type="submit" value="submit">
         </form>
         <p>You look like a:
-        <img src="/monster/monster.png"/>
-        '''.format(name)
+        <img src="/monster/{1}.png"/>
+        '''.format(name, name_hash)
     footer = '</body></html>'
 
     return header + body + footer
@@ -23,8 +33,13 @@ def mainpage():
 
 @app.route("/monster/<name>")
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+    image = cache.get(name)
+    if image is None:
+        print("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
+
     return Response(image, mimetype='image/png')
 
 
