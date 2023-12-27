c80414ee0f2b7fc1a5283f1276779b52ed1a8267
Cuong. Duong Manh <cuongpigerr@gmail.com>
2023-12-27 11:44:43 +0700

    This is Manh Cuong's commit 🌻.

---
+++ vngcloud/vcontainer-storage-interface/prod/docs/install.md
@@ -0,0 +1,3 @@
+# Service Account
+- Secret key: `b952f18d-79d9-4d56-90ab-f19e581c0435`
+- Client ID: `29dc8588-428d-4c8a-8ef9-b94a4ba880a6`
---
+++ vngcloud/vcontainer-storage-interface/prod/script/main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rehelm() {
+    helm uninstall -n kube-system vcontainer-storage-interface
+
+    rm -rf vcontainer-helm-infra
+
+    git clone https://github.com/vngcloud/vcontainer-helm-infra.git --branch stable --depth 1
+
+    helm install -n kube-system vcontainer-storage-interface vcontainer-helm-infra/vcontainer-storage-interface --values values.yaml
+
+    rm -rf vcontainer-helm-infra
+}
+
+loghelm() {
+    pod_id=$(kubectl get pods -n kube-system | grep vcontainer-storage-interface-controllerplugin | awk '{print $1}')
+    kubectl logs -n kube-system -f $pod_id vcontainer-storage-interface
+}
+
+opt=$1
+case $opt in
+rehelm)
+    rehelm
+    ;;
+loghelm)
+    loghelm
+    ;;
+*)
+    echo "Usage: $0 {rehelm|loghelm}"
+    exit 1
+    ;;
+esac
